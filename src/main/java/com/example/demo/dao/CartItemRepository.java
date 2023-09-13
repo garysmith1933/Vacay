ed867bfd0f359514f211ffd448aa6d2de692d8e3
Gary Smith <gsmi572@wgu.edu>
2023-09-13 11:35:00 -0700

    prevented customer fields from being null

--- src/main/java/com/example/demo/dao/CartItemRepository.java
+++ src/main/java/com/example/demo/dao/CartItemRepository.java
@@ -7,7 +7,3 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin(origins = "http://localhost:4200")
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 }
-
-//You just finished task e, but you need to make sure everything works.
-//More than likely you will need to adjust the column names to fit the database
-//if any errors were to occur.
