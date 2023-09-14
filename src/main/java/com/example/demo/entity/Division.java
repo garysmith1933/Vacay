e1a1e7a1fd81c31663b76b087c003d2a34617792
Gary Smith <gsmi572@wgu.edu>
2023-09-13 17:52:23 -0700

    fixed bug that prevented divisions being shown on front end

--- src/main/java/com/example/demo/entity/Division.java
+++ src/main/java/com/example/demo/entity/Division.java
@@ -18,17 +18,29 @@ public class Division {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="division_id")
     private Long id;
+
     @Column(name="division")
     private String division_name;
+
     @Column(name="create_date")
     @CreationTimestamp
     private Date createDate;
+
     @UpdateTimestamp
     @Column(name="last_update")
     private Date lastUpdate;
-    @ManyToOne
-    @JoinColumn(name="country_id")
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="country_id", insertable = false, updatable = false)
     private Country country;
+
+    @Column(name = "country_id")
+    private Long country_id;
+    public void setCountry(Country country){
+        setCountry_id(country.getId());
+        this.country = country;
+    }
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "division")
     private Set<Customer> customers = new HashSet<>();
 }
