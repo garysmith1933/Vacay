68d1638800539d75f7910c3f87463dad976b298f
Gary Smith <garysmith193358@gmail.com>
2024-03-19 14:32:26 -0400

    created new route for single vacation and started on vacation component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import ErrorPage from './components/error-page.tsx';
+import Vacation from './components/Vacation.tsx';
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <App/>,
-  errorElement: <ErrorPage/>
-}])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: '/vacation', //need to change to '/vacation/:vacationId' for single vacation
+    element: <Vacation/>,
+    errorElement: <ErrorPage/>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
