5de220a6f2416600fed4afaf541438dec817b307
Gary Smith <garysmith193358@gmail.com>
2024-03-19 14:14:47 -0400

    created error page component to handle errors

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import ErrorPage from './components/error-page.tsx';
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([{
   path: "/",
   element: <App/>,
+  errorElement: <ErrorPage/>
 }])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
