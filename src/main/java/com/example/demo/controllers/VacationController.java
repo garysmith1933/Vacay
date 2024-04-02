70a181bbc78da264f4cdb6e4331ac60082381292
Gary Smith <garysmith193358@gmail.com>
2024-04-02 16:30:52 -0400

    finished adding code for fetch vacations in vaction controller

--- src/main/java/com/example/demo/controllers/VacationController.java
+++ src/main/java/com/example/demo/controllers/VacationController.java
@@ -2,7 +2,11 @@
 @RestController
 @RequestMapping("api/vacations")
 public class VacationController {
+
+  @Autowired
+  private VacatiionService vacationService; //need to import later
   
   public List<Vacation> fetch_vacations() {
+    return vacationService.fetch_vacations();
   }
 }
