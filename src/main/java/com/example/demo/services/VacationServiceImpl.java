ffdb81e8d5ed3634dea4c901ec83dd53171363fc
Gary Smith <garysmith193358@gmail.com>
2024-04-08 14:02:33 -0400

    added imports for vacation service implementation

--- src/main/java/com/example/demo/services/VacationServiceImpl.java
+++ src/main/java/com/example/demo/services/VacationServiceImpl.java
@@ -1,4 +1,14 @@
+package com.example.demo.services;
 
+import com.example.demo.dao.VacationRepository;
+import com.example.demo.entity.Vacation;
+import com.example.demo.services.VacationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class VacationServiceImpl implements VacationService {
 
   @Autowired
